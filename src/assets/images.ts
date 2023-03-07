d69f7e2c2dd244d21ad4844f8f444a90580fcfe8
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2023-03-07 09:04:54 +0530

    commit - assetsUpdate

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,1 +1,3 @@
-exports();
+import ShopLogo from "./shopLogo.png"
+import CartLogo from "./CartLogo.png"
+export { ShopLogo, CartLogo}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,38 @@
+import ShoppingBagOutlined from "@mui/icons-material/ShoppingBagOutlined"
+import { ShopLogo } from "../assets/images"
 
 export default function Navbar() {
     return (
         <div className="w-full h-20 bg-white/80 border-b-[0.05rem] border-gray-700">
-            <div className="">
-                
+            <div className="border max-w-screen-xl h-full mx-auto flex items-center justify-between">
+                <div>
+                    <img src={ShopLogo} className="w-16" alt="PageLogo" />
+                </div>
+
+                <div>
+                    <ul className="flex items-center gap-8 ">
+                        <li className="text-base text-black font-bold hover:text-red-600 
+                        hover:underline underline-offset-2 decoration-[0.1rem] 
+                        cursor-pointer">Home</li>
+                        <li className="text-base text-black font-bold hover:text-red-600 
+                        hover:underline underline-offset-2 decoration-[0.1rem] 
+                        cursor-pointer">Pages</li>
+                        <li className="text-base text-black font-bold hover:text-red-600 
+                        hover:underline underline-offset-2 decoration-[0.1rem] 
+                        cursor-pointer">Shop</li>
+                        <li className="text-base text-black font-bold hover:text-red-600 
+                        hover:underline underline-offset-2 decoration-[0.1rem] 
+                        cursor-pointer">Element</li>
+                        <li className="text-base text-black font-bold hover:text-red-600 
+                        hover:underline underline-offset-2 decoration-[0.1rem] 
+                        cursor-pointer">Blog</li>
+                    </ul>
+                    <div>
+                        <ShoppingBagOutlined />
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
