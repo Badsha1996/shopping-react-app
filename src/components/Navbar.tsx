7b6462532c3483ba95171367b56a980986c208c1
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2023-03-07 08:28:55 +0530

    commit - logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,9 @@
 export default function Navbar() {
     return (
         <div className="w-full h-20 bg-white/80 border-b-[0.05rem] border-gray-700">
-            <div className=""></div>
+            <div className="">
+                
+            </div>
         </div>
     )
 }
