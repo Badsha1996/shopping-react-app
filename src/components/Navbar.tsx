30b2755cd00b22b54cf1e39a5f4392e46753a896
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2023-03-07 10:06:14 +0530

    commit - Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,24 @@ import { CartLogo, ShopLogo } from "../assets/images"
 export default function Navbar() {
     return (
         <div className="w-full h-20 bg-white/80 border-b-[0.05rem] border-gray-700">
-            <div className="border max-w-screen-xl h-full mx-auto flex items-center justify-between">
+            <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
                 <div>
-                    <img src={ShopLogo} className="w-16" alt="PageLogo" />
+                    <img src={ShopLogo} className="w-16 mix-blend-normal" alt="PageLogo" />
                 </div>
 
-                <div>
-                    <ul className="flex items-center gap-8 ">
+                <div className="flex items-center gap-8 ">
+                    <div className="relative">
+                        <img src={CartLogo} className="w-8 " alt="shoppingCart" />
+                        <span className="absolute w-4 h-4 top-0 left-4 text-sm flex 
+                        items-center text-white rounded-full bg-red-600 justify-center font-semibold">4</span>
+                    </div>
+                    <div>
+                        <img className="w-8 rounded-full"  src="https://lh3.googleusercontent.com/T14Y9Ms8bvvRg5ivZPjoNDJMZ8TIrkaEuUu6xhKLzabfWkTRqBXmooPTWqsdbmO7_Y288bAPl1bwG_NeAU7Lt5Q3lwibbZz7PnDvGLV2ciG2LQE7=w380-nu-rj-l70-e365" alt="profileLogo" />
+                    </div>
+                    <ul className="p-4 md:flex items-center gap-8 ">
+                        <li className="text-base text-black font-extrabold hover:text-red-600 
+                        hover:underline underline-offset-2 decoration-[0.1rem] 
+                        cursor-pointer ml-8 mt-20 md:hidden">X</li>
                         <li className="text-base text-black font-bold hover:text-red-600 
                         hover:underline underline-offset-2 decoration-[0.1rem] 
                         cursor-pointer">Home</li>
@@ -26,9 +37,6 @@ export default function Navbar() {
                         hover:underline underline-offset-2 decoration-[0.1rem] 
                         cursor-pointer">Blog</li>
                     </ul>
-                    <div>
-                        <img src={CartLogo} alt="" />
-                    </div>
                 </div>
             </div>
 
