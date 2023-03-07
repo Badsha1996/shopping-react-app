9e3108f3baf981b920fdd8be67a11e3a090a3de2
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2023-03-07 08:21:42 +0530

    commit - navbarStyles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className="w-full h-20 bg-white/80 border-b-[0.05rem] border-gray-700">
+            <div className=""></div>
+        </div>
+    )
 }
