9e740d123ee8957f1d6666863cbb0c854cedc855
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2023-03-12 22:06:18 +0530

    Commit Hamburder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ export default function Navbar() {
     const [isOpen, setOpen] = useState(false)
 
     return (
-        <div className="sticky z-50 top-0 w-screen h-20 bg-white border-b-[0.05rem]  border-gray-700">
-            <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
+        <div className="sticky z-50 top-0 h-20 bg-white border-b-[0.05rem] border-gray-700 ">
+            <div className="  h-full mx-auto  flex items-center justify-between">
                 <Link to={"/"}>
                     <div className="flex items-center">
                         <img src={ShopLogo} className="w-16 mix-blend-normal" alt="PageLogo" />
@@ -30,7 +30,7 @@ export default function Navbar() {
 
 
 
-                    <ul className={` hidden 
+                    <ul className={` hidden w-full
                 mt-36   md:mt-auto   md:flex items-center gap-8 md:bg-white`}>
                         <li className=" text-base text-black font-bold hover:text-white md:hover:text-red-600
                     hover:underline underline-offset-2 decoration-[0.1rem] 
@@ -63,17 +63,18 @@ export default function Navbar() {
             </div>
 
 
-            <div className={`${isOpen == false ? 'translate-y-[50rem]' : 'translate-y-[0rem]'} h-[81vh] w-full top-[0rem] z-20  md:hidden  relative transition-transform duration-500 flex items-center
+            <div className={`${isOpen == false ? 'translate-y-[50rem]' : 'translate-y-[0rem]'} h-[81vh] w-full top-[0rem] z-20  md:hidden  relative  transition-transform duration-500 flex items-center
             justify-center
-            bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500
+            bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900
             
             `}>
                 <img src={Hero} alt="navbar image" className=" clip-path w-full z-10  h-full absolute opacity-[0.8]" />
 
 
-                <div className="w-full flex gap-2 ml-2 z-30">
-                    <ul className=" flex flex-col gap-14 ">
-                        <li className="border-white/20  border border-t-0 border-l-0 border-r-0 py-4 px-16 
+                <div className="w-full flex gap-2 items-center justify-center ml-2 z-30">
+                    <ul className=" flex flex-col items-end gap-14 ">
+                        
+                        <li className="border-white/20 bg-gray-400/20  border border-t-0 border-l-0 border-r-0 py-4 px-16 
                         text-white
                         hover:bg-white 
                         hover:text-black
@@ -91,19 +92,7 @@ export default function Navbar() {
                         hover:border-gray-400 duration-500 font-semibold">Shop</li>
                     </ul>
 
-                    <div className=" flex flex-col-reverse w-full ">
-                        
-                        <div className="p-4 flex flex-col justify-center items-center">
-                            <h2 className=" text-base text-black font-bold">About us</h2>
-                            <p className="text-sm ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt vero iusto totam vitae! Aut, ipsam? </p>
-                            <p className="text-sm font-semibold">Ph: +90 82937404</p>
-                        </div>
-                        <div className="flex gap-1 items-center justify-center font-semibold text-white ">
-                            <BsShare className="bg-black/60 rounded-full h-6 w-6 py-2 px-2" />
-                            <BsActivity className="bg-black/60 rounded-full h-6 w-6 py-2 px-2" />
-                            <BsBadge4K className="bg-black/60 rounded-full h-6 w-6 py-2 px-2" />
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         </div>
